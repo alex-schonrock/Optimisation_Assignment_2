a7602868f00542b411f3b9187dcc47cd2752202a
alex-schonrock <alex@schonrocks.com>
2025-11-23 18:45:43 +0100

    started

--- Data_ops/data_loader.py
+++ Data_ops/data_loader.py
@@ -1,3 +1,17 @@
-#### data loader class
+import pandas as pd
+from pathlib import Path
 
+from pathlib import Path
+from dataclasses import dataclass
+from logging import Logger
+import pandas as pd
+from Utils.utils import load_datafile
+#### data loader class
+class DataLoader:
+    def __init__(self, input_path: str, model_type: str):
+        self.input_path = Path(input_path).resolve()
+        self.model_type = model_type
+    def load_data_file(self, file_name: str):
+        data = load_datafile(file_name, self.input_path)
+        return data
 
--- Utils/utils.py
+++ Utils/utils.py
@@ -1,6 +1,16 @@
-### load dataset function
-
-
+import json
+import csv
+import pandas as pd
+from pathlib import Path
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+import numpy as np
 ### load data file function
+def load_datafile(file_name, input_path):
+    base_path = Path(input_path) / file_name
+    result = pd.read_csv(base_path, sep=";")
+    # print(result)
+    return result
+
 
 ### plotting functions
